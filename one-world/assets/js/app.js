678b8e18406e3d2fa6c5848aeec44b2c8131b9d5
lewejuice <hamiltonlewis@hotmail.com>
2020-03-31 10:10:01 +0000

    adding buttons and targets

--- one-world/assets/js/app.js
+++ one-world/assets/js/app.js
@@ -125,3 +125,20 @@ function myFunction8() {
     moreText.style.display = "inline";
   }
 }
+
+mybutton = document.getElementById("myBtnb");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
